930cfeb2741ac1ecd7d44dca86d54534c23d78bf
Zavim <Zark471@gmail.com>
2022-12-02 12:35:16 -0500

    -f layout complete

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Guardian Medical Equipment';
+export const SITE_DESCRIPTION = 'Welcome to Guardian Medical Equipmentt!';
