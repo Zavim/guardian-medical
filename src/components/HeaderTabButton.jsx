14d61c77a9413e1af9f7400fa4af12658331b433
Zavim <Zark471@gmail.com>
2022-12-09 16:35:11 -0500

    -c fix img width issue, polish

--- src/components/HeaderTabButton.jsx
+++ src/components/HeaderTabButton.jsx
@@ -11,14 +11,25 @@ export default function HeaderTab(props) {
           setOpen(!open);
         }}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="1em"
-          height="1.2em"
-          fill="#ffffff"
-        >
-          <path d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
-        </svg>
+        {open ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1.2em"
+            fill="#ffffff"
+          >
+            <path d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1.2em"
+            fill="#ffffff"
+          >
+            <path d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+          </svg>
+        )}
       </button>
       {open ? props.children : <></>}
     </div>
