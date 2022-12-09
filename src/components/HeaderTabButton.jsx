aed77ae9a08f002d5c8e50fd80a817e06b961bef
Zavim <Zark471@gmail.com>
2022-12-09 16:04:58 -0500

    -f add mobile nav toggle

---
+++ src/components/HeaderTabButton.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function HeaderTab(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="header-button-container">
+      <button
+        className="header-button"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1.2em"
+          fill="#ffffff"
+        >
+          <path d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+        </svg>
+      </button>
+      {open ? props.children : <></>}
+    </div>
+  );
+}
